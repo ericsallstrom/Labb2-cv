4f139151b3743c9bf5c1509878f581cb3e1b2f67
ericsallstrom <eric.sallstrom@gmail.com>
2024-02-21 08:28:10 +0100

    fix/size for smaller screen ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,11 @@ import '../pages/home-style.css';
 
 const Home = () => {
     return (
-        <div className="home-wrapper">
-            <div className="home-content">
-                <div className="hex-container">
-                    <div className="hexagon top"></div>
-                    <div className="hexagon left"></div>
-                    <div className="hexagon right"></div>
-                </div>
+        <div className="home-content">
+            <div className="hex-container">
+                <div className="hexagon top"></div>
+                <div className="hexagon left"></div>
+                <div className="hexagon right"></div>
                 <h2 className="home-title">Eric Sällström - Software Engineer</h2>
             </div>
         </div>
